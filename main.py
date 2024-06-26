0b4af5686ccd6b6079b60d8af50c950163eec68f
Arnav <tellarnav3@gmail.com>
2024-06-27 03:42:51 +0530

    Added More Endpoints

--- main.py
+++ main.py
@@ -4,22 +4,50 @@ import pandas as pd
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("home.html")
+    df = pd.read_csv("data_small/stations.txt", skiprows=17)
+    main_df = df[["STAID", "STANAME                                 "]]
+    return render_template("home.html", data = main_df[0:92])
 
 
 @app.route("/api/v1/<station>/<date>")
 def data(station, date):
-    temperature = "25"
     no_of_zero = 6 - len(station)
     station = int(station)
     df = pd.read_csv(f"data_small/TG_STAID{no_of_zero*'0'}{station}.txt", skiprows=20, parse_dates=["    DATE"])
     temp = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
 
     return {"station": station,
-            "date": date,
-            "temperature": temp}
+            "temperature": temp,
+            "date": date,}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    data = []
+    no_of_zero = 6 - len(station)
+    station = int(station)
+    df1 = pd.read_csv(f"data_small/TG_STAID{no_of_zero*'0'}{station}.txt", skiprows=20, parse_dates=["    DATE"])
+    df = df1[["   TG", "    DATE", "STAID"]]
+    df.columns = ["date", "station", "temperature"]
+
+    data = df.to_dict(orient="records")
+    return data
+
+@app.route("/api/v1/<station>/yearly/<year>")
+def all_data_yearly(station, year):
+    no_of_zero = 6 - len(station)
+    station = int(station)
+    df1 = pd.read_csv(f"data_small/TG_STAID{no_of_zero*'0'}{station}.txt", skiprows=20,)
+    df = df1[["   TG", "    DATE", "STAID"]]
+    df["    DATE"] = df["    DATE"].astype(str)
+    print(df)
+    wanted_df = df.loc[df["    DATE"].str.startswith(str(year))]
+    wanted_df.columns = ["date", "station", "temperature"]
+    print(wanted_df)
+    data = wanted_df.to_dict(orient="records")
+    return data
 
 
 if __name__ == "__main__":
